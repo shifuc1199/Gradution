22b951339f95c98c18289c9930eba0583ee3eced
shifuc1199 <745490282@qq.com>
2019-11-21 23:30:40 +0800

    2.0

--- GraduationProject/Assets/Scripts/Player/PlayerAnimationEvent.cs
+++ GraduationProject/Assets/Scripts/Player/PlayerAnimationEvent.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class PlayerAnimationEvent : MonoBehaviour
 {
+    public GameObject sword_slash;
+    public GameObject Huan_Ying;
     public void ShootBullet() //远程攻击
     {
         Instantiate(Resources.Load<GameObject>("Bullet"), GetComponent<PlayerController>()._shoot_pos.position, transform.rotation);
     }
-
+    public void SetSlash()
+    {
+        transform.DOMoveX(transform.right.x * 8 + transform.position.x, 0.1f).SetEase(Ease.Linear);
+        sword_slash.SetActive(true);
+        Huan_Ying.SetActive(true);
+        Timer.Register(0.6f, () => { Huan_Ying.SetActive(false); sword_slash.SetActive(false); });
+    }
     public void OnAttackEnter()
     {
 
